35124418f44dd9d325ea06e8e5a6c81e0a2b691d
luckyroy <107039265+luckyroy5615@users.noreply.github.com>
2022-10-02 12:08:55 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,69 +3,83 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-var ball,groundObj,leftSide,rightSide;
+var ball, groundObj, leftSide, rightSide;
 var world;
+var dustbinImg, paperImg;
 var radius = 70;
 
-function preload(){
-//find the bug in the below code
-	//dustbinImg = addImage("dustbin.png");
-	//paperImg = addImage("paper.png");
+function preload() {
+  //find the bug in the below code
+  dustbinImg = loadImage("dustbin.png");
 
+
+  paperImg = loadImage("paper.png");
+
+
+  //keyPressed(UP_ARROW)
+  if(keyPressed(UP_ARROW))
+  //fill(225)
+fill(225)
 }
 
 
 function setup() {
-	createCanvas(1600, 700);
-	rectMode(CENTER);
+  createCanvas(1600, 700);
+  rectMode(CENTER);
 
-	engine = Engine.create();
-	world = engine.world;
+  engine = Engine.create();
+  world = engine.world;
 
-	var ball_options={
-		isStatic:false,
-		restitution:0.3,
-		density:0.4
-	}
 
-	ball = Bodies.circle(260,100,radius/2.6,ball_options);
-	World.add(world,ball);
+  var ball_options = {
+    isStatic: false,
+    restitution: 0.3,
+    density: 0.4
+  }
 
-	ground=new Ground(width/2,670,width,20);
-	leftSide = new Ground(1100,600,10,120);
-	rightSide = new Ground(1270,600,10,120);
-	bottomSide = new Ground(1185,650,150,20);
+  ball = Bodies.circle(260, 100, radius / 2.6, ball_options);
+  World.add(world, ball);
 
-	Engine.run(engine);
-  
+  ground = new Ground(width / 2, 670, width, 20);
+  leftSide = new Ground(1100, 600, 10, 120);
+  rightSide = new Ground(1270, 600, 10, 120);
+  bottomSide = new Ground(1185, 650, 150, 20);
+
+  Engine.run(engine);
+
+
+
+
+
+
 }
 
 
 function draw() {
-	background(200);
-	rectMode(CENTER);
+  background(200);
+  rectMode(CENTER);
 
 
-	ground.display();
-	leftSide.display();  
-	rightSide.display();
-	bottomSide.display();
+  ground.display();
+  leftSide.display();
+  rightSide.display();
+  bottomSide.display();
 
-	
-	imageMode(CENTER);
-		//use image() command to add paper image to the ball
-image(,ball.position.x,ball.position.y,radius,radius);
 
-	// use image() command to add dustbin image in the canvas.
-	image(,1185, 570, 200,200);
-	
+  //imageMode(CENTER);
+  //use image() command to add paper image to the ball
+  image(paperImg, ball.position.x, ball.position.y, radius, radius);
+
+  // use image() command to add dustbin image in the canvas.
+  image(dustbinImg, 1089, 470, 200, 200);
+
 
 }
 
 function keyPressed() {
-  	if (keyCode === UP_ARROW) {
+  if (keyCode === UP_ARROW) {
 
-		Matter.Body.applyForce(ball,ball.position,{x:85,y:-85});
-    
-  	}
+    Matter.Body.applyForce(ball, ball.position, { x: 85, y: -85 });
+
+  }
 }
